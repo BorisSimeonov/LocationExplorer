e60a244a1ab19309538cf1104b8a5b671d2a87e6
Boris Simeonov <twistbobg@gmail.com>
2017-12-22 11:57:17 +0200

    Added 'Under development' tooltips for unfinished features

--- LocationExplorer.Web/wwwroot/js/site.js
+++ LocationExplorer.Web/wwwroot/js/site.js
@@ -1,25 +1,5 @@
-﻿//(function ($) {
-//    $(document).ready(() => {
-//        $(".delete-btn").click((e) => {
-//            e.preventDefault();
-//            let targetHref = $(e.target).attr("href");
-//            let result = confirm("Are you sure you want to delete this item?");
-//            if (result === true) {
-//                $(location).attr("href", targetHref);
-//            };
-//        });
-//    });
-//})(jQuery);
-
-
-
-// Select all elements with data-toggle="tooltips" in the document
-//$('[data-toggle="tooltip"]').tooltip();
-
-//$(document).ready(function () {
-//    $('span.glyphicon-eye-open').tooltip({ title: "Read", container: "a" });
-//});
-
-//$(document).ready(function () {
-//    $('a.td-details-link').tooltip({ title: "View Details", container: "td" });
-//});
+﻿$(document).ready(function () {
+    let underDevelopmentMessage = 'This feature is under development.';
+    $('a.not-implemented').tooltip({ title: underDevelopmentMessage, container: "td" });
+    $('button.upload-missing').tooltip({ title: underDevelopmentMessage, container: "form" });
+});
