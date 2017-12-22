e9c1cd8ca5efac1be12abfefb6a4008b8415ed26
Boris Simeonov <twistbobg@gmail.com>
2017-12-22 18:26:17 +0200

    Small refactoring over paging tag helper

--- LocationExplorer.Web/Infrastructure/TagHelpers/PagingTagHelper.cs
+++ LocationExplorer.Web/Infrastructure/TagHelpers/PagingTagHelper.cs
@@ -37,19 +37,25 @@
 
             for (int i = 1; i <= PageModel.TotalPages; i++)
             {
-                TagBuilder tag = new TagBuilder("a");
-                tag.Attributes["href"] = urlHelper.Action(PageAction, new { page = i });
-                if (PageClassesEnabled)
-                {
-                    tag.AddCssClass(PageClass);
-                    tag.AddCssClass(i == PageModel.CurrentPage
-                        ? PageClassSelected : PageClassNormal);
-                }
-                tag.InnerHtml.Append(i.ToString());
-                result.InnerHtml.AppendHtml(tag);
+                result.InnerHtml.AppendHtml(CreateDivContent(i, urlHelper));
             }
 
             output.Content.AppendHtml(result.InnerHtml);
+        }
+
+        private TagBuilder CreateDivContent(int pageIdx, IUrlHelper urlHelper)
+        {
+            TagBuilder tag = new TagBuilder("a");
+            tag.Attributes["href"] = urlHelper.Action(PageAction, new { page = pageIdx });
+            if (PageClassesEnabled)
+            {
+                tag.AddCssClass(PageClass);
+                tag.AddCssClass(pageIdx == PageModel.CurrentPage
+                    ? PageClassSelected : PageClassNormal);
+            }
+            tag.InnerHtml.Append(pageIdx.ToString());
+
+            return tag;
         }
     }
 }
